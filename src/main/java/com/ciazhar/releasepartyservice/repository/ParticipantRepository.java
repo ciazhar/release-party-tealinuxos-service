9d5387537870411a79cb58c6f1f2d399390fbce7
ciazhar <ciazhar.id@gmail.com>
2017-12-01 20:29:11 +0700

    make repository

---
+++ src/main/java/com/ciazhar/releasepartyservice/repository/ParticipantRepository.java
@@ -0,0 +1,10 @@
+package com.ciazhar.releasepartyservice.repository;
+
+import com.ciazhar.releasepartyservice.model.Participant;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+/**
+ * Created by ciazhar on 01/12/17.
+ */
+public interface ParticipantRepository extends ReactiveMongoRepository<Participant,String> {
+}
