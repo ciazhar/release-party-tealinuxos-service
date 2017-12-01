5b937cd22a5dc05c01a19b11da27cec310f25026
ciazhar <ciazhar.id@gmail.com>
2017-12-01 20:04:11 +0700

    initial commit

---
+++ src/main/java/com/ciazhar/releasepartyservice/ReleasePartyServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ciazhar.releasepartyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleasePartyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleasePartyServiceApplication.class, args);
+	}
+}
