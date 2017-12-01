0ab439203c03654e43a183490ec945b083420fe8
ciazhar <ciazhar.id@gmail.com>
2017-12-01 21:07:00 +0700

    add attend form

---
+++ src/main/java/com/ciazhar/releasepartyservice/model/request/AttendForm.java
@@ -0,0 +1,14 @@
+package com.ciazhar.releasepartyservice.model.request;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * Created by ciazhar on 01/12/17.
+ * [ Documentatiion Here ]
+ */
+@Data
+@Builder
+public class AttendForm {
+    private String participantId;
+}
